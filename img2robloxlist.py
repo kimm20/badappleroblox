30fe91baa744d791bb7d44cbb7247b5b2cf7430b
Kimm20 kimm20 <kimsiri1710@gmail.com>
2021-04-22 10:11:39 +0700

    a

---
+++ img2robloxlist.py
@@ -0,0 +1,34 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+cap = cv2.VideoCapture("video.mp4")
+data=[]
+#data[t][y][x]
+
+while True:
+    ret,frame=cap.read()
+    if ret:
+        
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        frame = cv2.resize(frame,(44,25))
+        frame = np.array(frame)/255
+        data.append(frame)
+            
+    else:
+        break
+cv2.destroyAllWindows()
+file=open("pros.txt","w+")
+ee=[]
+for i in range(len(data)):
+    oo=data[i].tolist()
+    for y in range(len(oo)):
+        for x in range(len(oo[y])):
+            oo[y][x]=np.round(oo[y][x],2)
+                
+    ee.append(oo)
+ee = str(ee)
+ee = ee.replace("[","{")
+ee = ee.replace("]","}")
+
+file.write(ee)
+file.close()
